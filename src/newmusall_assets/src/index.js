3ba7439f1f80a069885735e0e9ec41954fcb09ab
fleeB1 <fowler.lee8@gmail.com>
2022-07-27 20:04:07 +0200

    Initial commit.

---
+++ src/newmusall_assets/src/index.js
@@ -0,0 +1,19 @@
+import { newmusall } from "../../declarations/newmusall";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await newmusall.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
